5208d8a6b9733e0fa6e5514ee9f874ce5f6857e9
Hasan Can Yıldız <hyildiz955@gmail.com>
2026-08-08 23:23:26 +0300

    fix some pronlems

--- src/test/java/com/ecommerce/ecommerce_app/EcommerceAppApplicationTests.java
+++ src/test/java/com/ecommerce/ecommerce_app/EcommerceAppApplicationTests.java
@@ -2,17 +2,11 @@ package com.ecommerce.ecommerce_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class EcommerceAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
-
 }
